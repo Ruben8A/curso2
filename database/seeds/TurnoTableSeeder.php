b24b9c9e80106815dbf47e4b6d60ed6fb65aab20
Ruben Ochoa Rodriguez <rochoa@fiscaliaveracruz.gob.mx>
2018-03-17 12:14:42 -0600

    Subiendo Seed

---
+++ database/seeds/TurnoTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Turno;
+class TurnoTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $turnos = array (
+        	['descripcion' => 'MATUTINO'],
+        	['descripcion' => 'VESPERTINO'],
+        	['descripcion' => 'NOCTURNO'],
+        	['descripcion' => 'JORNADA ACUMULADA'],
+        	['descripcion' => 'ESPECIALES'],
+        	['descripcion' => 'SABADO'],
+        	['descripcion' => 'DOMINGO'],
+        	['descripcion' => '24 x 24']
+        );
+
+        foreach($turnos as $turno){
+        	Turno::create($turno);
+        }
+    }
+}
