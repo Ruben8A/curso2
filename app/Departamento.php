ed5846ccb2b2e9becbc95a4eb30f720489c37845
Ruben Ochoa Rodriguez <rochoa@fiscaliaveracruz.gob.mx>
2018-03-17 10:53:57 -0600

    Creando modelos

---
+++ app/Departamento.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Departamento extends Model
+{
+    //
+    protected $table = 'cat_departamentos';
+    protected $fillable =['codigo','descripcion'];
+
+    public function empleados(){
+    	return  $this->hasMany('App\Empleado','id_departamento','id');
+    }
+
+    /*public function setDescriptionAttribute($value)
+    {
+    	$this->attributes['descripcion'] = mb_strtoupper($value,'utf-8');
+    }*/
+}
---
+++ app/Turno.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Turno extends Model
+{
+    //
+    protected $table = 'cat_turnos';
+    protected $fillable =['descripcion'];
+
+    public function empleados(){
+    	return  $this->hasMany('App\Empleado','id_turno','id');
+    }
+
+    /*public function setDescriptionAttribute($value)
+    {
+    	$this->attributes['descripcion'] = mb_strtoupper($value,'utf-8');
+    }*/
+}
